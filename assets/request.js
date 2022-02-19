f070a4846b357dd7ab5246fecfff9de840c5c525
chenjunbin <jokerchan2139@gmail.com>
2022-02-19 15:27:36 +0800

    requset

--- assets/request.js
+++ assets/request.js
@@ -1,17 +1,55 @@
- function postData(url = '', data = {}) {
-    fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-            'siteValue':'hyfol',
-            'user-agent': navigator.userAgent
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
+function ajax_method(url,data,method,success) {
+    // 异步对象
+    var ajax = new XMLHttpRequest();
+
+    // get 跟post  需要分别写不同的代码
+    if (method==='get') {
+        // get请求
+        if (data) {
+            // 如果有值
+            url+='?';
+            url+=data;
+        }else{
+
+        }
+        // 设置 方法 以及 url
+        ajax.open(method,url);
+
+        // send即可
+        ajax.send();
+    }else{
+        // post请求
+        // post请求 url 是不需要改变
+        ajax.open(method,url);
+
+        // 需要设置请求报文
+        ajax.setRequestHeader("Content-type","application/json");
+        ajax.setRequestHeader("siteValue","hyfol");
+        // 判断data send发送数据
+        if (data) {
+            // 如果有值 从send发送
+            ajax.send(JSON.stringify(data));
+        }else{
+            // 木有值 直接发送即可
+            ajax.send();
+        }
+    }
+
+    // 注册事件
+    ajax.onreadystatechange = function () {
+        // 在事件中 获取数据 并修改界面显示
+        if (ajax.readyState===4&&ajax.status===200) {
+            // console.log(ajax.responseText);
+
+            // 将 数据 让 外面可以使用
+            // return ajax.responseText;
+
+            // 当 onreadystatechange 调用时 说明 数据回来了
+            // ajax.responseText;
+
+            // 如果说 外面可以传入一个 function 作为参数 success
+            success(ajax.responseText);
+        }
+    }
+
 }
