4df2339d445bb554016a79100ee6556a4cdd3181
chenjunbin <jokerchan2139@gmail.com>
2022-02-19 14:48:45 +0800

    requset

--- assets/request.js
+++ assets/request.js
@@ -1,5 +1,5 @@
-async function postData(url = '', data = {}) {
-    let response = await fetch(url, {
+ function postData(url = '', data = {}) {
+    fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -14,5 +14,4 @@ async function postData(url = '', data = {}) {
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
-    return response.json(); // parses JSON response into native JavaScript objects
 }
