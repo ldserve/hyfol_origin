7d6dd1accdf387127d7c1ec3e13f6c91ef4a00dc
chenjunbin <jokerchan2139@gmail.com>
2022-02-19 14:45:51 +0800

    no message

--- assets/request.js
+++ assets/request.js
@@ -1,5 +1,5 @@
 async function postData(url = '', data = {}) {
-    const response = await fetch(url, {
+    let response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
